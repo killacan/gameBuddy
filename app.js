160cf97e67bb5706613029aa73e3f92ce4256c6d
mattzteh <mattzteh@gmail.com>
2022-09-19 09:50:47 -0700

    create csrf validation for non-get requests

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const debug = require('debug');
+const cors = require('cors');
+const csurf = require('csurf');
+const path = require('path');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/api/users');
+const csrfRouter = require('./routes/api/csrf')
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+const { isProduction } = require('./config/keys')
+
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+)
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
