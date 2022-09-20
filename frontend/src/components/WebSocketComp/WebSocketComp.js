85bdaa5068bcc7f9b8928b5e214740138a6b2c6d
killacan <kameron000@yahoo.com>
2022-09-20 10:44:52 -0700

    adding websocket server

--- frontend/src/components/WebSocketComp/WebSocketComp.js
+++ frontend/src/components/WebSocketComp/WebSocketComp.js
@@ -8,7 +8,9 @@ function WebSocketComp () {
         console.log('Websocket is connected!')
         const id = Math.round(Math.random() * 100)
         console.log('sending...', id)
-        const data = JSON.stringify({
+        const data = JSON.stringify(
+        [
+            {
             id,
             name: `[${id}] Jimmothy`,
             address: {
@@ -17,13 +19,39 @@ function WebSocketComp () {
                 state: 'CA'
             },
             profession: 'developer'
-        })
+        },
+        {
+            id,
+            name: `[${id}] Juan`,
+            address: {
+                street: '123 Main St',
+                city: 'Anytown',
+                state: 'CA'
+            },
+            profession: 'developer'
+        },
+        {
+            id,
+            name: `[${id}] Marry Santos`,
+            address: {
+                street: '123 Main St',
+                city: 'Anytown',
+                state: 'CA'
+            },
+            profession: 'developer'
+        }])
 
         socket.send(data)
     }
-    socket.onmessage = (msg) => console.log('I got a message', msg);
+    socket.onmessage = (msg) => {
+        const message = JSON.parse(msg.data)
+        console.log('I got a message', message);
+        // messages.innerHTML += `<br/>${message}`
+    }
     socket.onerror = (error) => console.log(`WebSocket error: ${error}`);
     socket.onclose = (event) => console.log('Disconnected from WebSocket server');
+
+    if (!messages) return null
 
     return (
         <>
