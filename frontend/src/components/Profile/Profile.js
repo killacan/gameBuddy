d09c8de28d3c1ec872d2f855588d0f41a9ac4bb8
daniellien2018 <daniellien2018@gmail.com>
2022-09-20 16:21:02 -0700

    profile-route-complete

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,8 +1,24 @@
+import { useSelector } from 'react-redux';
 import './Profile.css'
 
 
 const Profile = () => {
+    const user = useSelector(state => state.session.user)
 
+    const {_id, username, email} = user
+    // console.log(user)
+    // console.log(user._id)
+    return(
+        <>
+        
+        <h1> Hello this is profile</h1>
+        <p>{_id}</p>
+        <p>{username}</p>
+        <p>{email}</p>
+        
+
+        </>
+    )
 }
 
 export default Profile;
