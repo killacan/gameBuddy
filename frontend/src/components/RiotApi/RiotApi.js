a4848b3fee18e170da4f05fd84161f7eeb5cd1d5
mattzteh <mattzteh@gmail.com>
2022-09-21 19:28:54 -0700

    implement rank fetch(unfinished)

--- frontend/src/components/RiotApi/RiotApi.js
+++ frontend/src/components/RiotApi/RiotApi.js
@@ -6,12 +6,13 @@ const RiotApi = () => {
     const RIOT_API_KEY = "RGAPI-84f95961-a2b7-45cc-b2c3-cf9ff92bf9cb";
     const [playerSearch, setPlayerSearch] = useState();
     const [playerData, setPlayerData] = useState({});
+    const [playerRank, setPlayerRank] = useState({});
 
     function searchForPlayer(event) {
-        const API_CALL_STRING = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" 
+        const API_CALL_PLAYER = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" 
         + playerSearch + "?api_key=" + RIOT_API_KEY;
 
-        axios.get(API_CALL_STRING).then(function(res) {
+        axios.get(API_CALL_PLAYER).then(function(res) {
             setPlayerData(res.data);
 
         }).catch(function(err) {
@@ -19,8 +20,21 @@ const RiotApi = () => {
         })
 
     }
-
     console.log(playerData)
+
+    function getRankForPlayer(event) {
+        const API_CALL_RANK = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/2yfFsNsshZzf3sIKcd05VFaRlYkSiF16Yy_bjXtIcN5iub8"
+        + "?api_key=" + RIOT_API_KEY;
+
+        axios.get(API_CALL_RANK).then(function(res) {
+            setPlayerRank(res.data);
+        }).catch(function(err) {
+            console.log(err)
+        })
+    }
+
+    console.log(playerRank)
+
     return (
         <>
         <div className="api-page">
@@ -41,6 +55,17 @@ const RiotApi = () => {
             :
                 <>
                 <h1>Player not found!</h1>
+                </>
+            }
+            <button onClick={e => getRankForPlayer(e)}>Get Rank</button>
+            {JSON.stringify(playerRank) != '{}' ?
+                <>
+                <h1>{playerRank}</h1>
+                <h1>hi</h1>
+                </>
+            :
+                <>
+                <h1>Rank not found!</h1>
                 </>
             }
         </div>
