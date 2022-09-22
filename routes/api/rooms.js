7f67ed43622ec1fea4601fa1728a2bec369d1596
mattzteh <mattzteh@gmail.com>
2022-09-22 11:33:40 -0700

    change push

--- routes/api/rooms.js
+++ routes/api/rooms.js
@@ -91,7 +91,7 @@ router.patch('/:roomId/join/:userId', requireUser, async(req, res, next) => {
         const user = await User.findById(req.params.userId);
 
         if (room) {
-            room.members = req.body.members.push(user) || room.members;
+            room.members = req.body.members.push(user._id) || room.members;
         }
         return res.json(room);
     }
