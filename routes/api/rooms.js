30fa5fe864ca34b9fc099e12c6e3007ee3677dad
mattzteh <mattzteh@gmail.com>
2022-09-22 11:18:36 -0700

    initialize member as empty array on create route room

--- routes/api/rooms.js
+++ routes/api/rooms.js
@@ -19,7 +19,7 @@ router.post('/create', requireUser, validateRoomInput, async (req, res, next) =>
     try {
         const newRoom = new Room({
             host: req.user._id,
-            members: [req.user._id],
+            members: [],
             title: req.body.title,
             game: req.body.game,
             duration: req.body.duration,
