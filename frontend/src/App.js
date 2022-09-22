d06a84dd474efed270dfead1b95689c95f2c1519
mattzteh <mattzteh@gmail.com>
2022-09-21 17:32:27 -0700

    create search bar for LoL players

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
 import Profile from "./components/Profile/Profile";
 import Rooms from "./components/Rooms/Rooms";
+import RiotApi from "./components/RiotApi/RiotApi";
 
 import MainPage from './components/MainPage/MainPage'
 import LoginForm from './components/SessionForms/LoginForm';
@@ -37,6 +38,7 @@ function App() {
         <ProtectedRoute exact path={"/profile"} component={Profile} />
         <ProtectedRoute exact path={"/games/rooms"} component={Rooms}/>
         <ProtectedRoute exact path={"/games/rooms/:roomId"} component={GameRoom} />
+        <ProtectedRoute exact path={"/riotapi"} component={RiotApi} />
 
         <Redirect to="/"/>
         
--- frontend/src/components/RiotApi/RiotApi.js
+++ frontend/src/components/RiotApi/RiotApi.js
@@ -5,13 +5,22 @@ const RiotApi = () => {
 
     const RIOT_API_KEY = "RGAPI-84f95961-a2b7-45cc-b2c3-cf9ff92bf9cb";
     const [playerSearch, setPlayerSearch] = useState();
+    const [playerData, setPlayerData] = useState({});
 
     function searchForPlayer(event) {
         const API_CALL_STRING = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" 
         + playerSearch + "?api_key=" + RIOT_API_KEY;
 
+        axios.get(API_CALL_STRING).then(function(res) {
+            setPlayerData(res.data);
+
+        }).catch(function(err) {
+            console.log(err)
+        })
+
     }
 
+    console.log(playerData)
     return (
         <>
         <div className="api-page">
@@ -22,7 +31,18 @@ const RiotApi = () => {
             type='text' 
             onChange={e => setPlayerSearch(e.target.value)} 
             />
-            <button onClick={e => searchForPlayer(event)}>Search</button>
+            <button onClick={e => searchForPlayer(e)}>Search</button>
+            {JSON.stringify(playerData) != '{}' ? 
+                <>
+                <h1>{playerData.name}</h1>
+                <img src={'http://ddragon.leagueoflegends.com/cdn/12.18.1/img/profileicon/' + playerData.profileIconId + ".png" }></img>
+                <h2>{playerData.summonerLevel}</h2>
+                </>
+            :
+                <>
+                <h1>Player not found!</h1>
+                </>
+            }
         </div>
         </>
     )
