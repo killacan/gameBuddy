f3e9951164a97dc459007bdc3be26a218b223c16
mattzteh <mattzteh@gmail.com>
2022-09-22 16:31:44 -0700

    create fetch api for profile page

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -17,81 +17,76 @@ const Profile = () => {
     const reviews = useSelector(state => state.session.user)
 
     const RIOT_API_KEY = process.env.REACT_APP_RIOT_API_KEY
-    const [playerSearch, setPlayerSearch] = useState();
-    const [playerName, setPlayerName] = useState({});
-    const [playerData, setPlayerData] = useState({});
-    
-    function searchForPlayer(event) {
-        const API_CALL_PLAYER = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" 
-        + riotUsername + "?api_key=" + RIOT_API_KEY;
 
-        axios.get(API_CALL_PLAYER).then(function(res) {
-            setPlayerName(res.data);
-        }).catch(function(err) {
-            console.log(err)
+    const [playerInfoComponent, setPlayerInfoComponent] = useState();
+    const [playerRankComponent, setPlayerRankComponent] = useState();
+
+    useEffect(() => {
+          
+        const result = fetch(("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" 
+        + riotUsername + "?api_key=" + RIOT_API_KEY), {method: 'GET'})
+        .then(res => res.json())
+        .then(data => {
+            const playerId = data.id;
+            if (playerId) {
+                setPlayerInfoComponent (
+                    <>
+                    <img src={'http://ddragon.leagueoflegends.com/cdn/12.18.1/img/profileicon/' + data.profileIconId + ".png" }></img>
+                    <h1>League Username:{data.name}</h1>
+                    <h2>Summoner Level:{data.summonerLevel}</h2>
+                    </>
+                )            
+            }
+            return fetch("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"
+            + playerId + "?api_key=" + RIOT_API_KEY)
         })
-    }
-    
-    const playerId = playerName.id;
-
-    function getRankForPlayer(event) {
-        const API_CALL_RANK = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"
-        + playerId + "?api_key=" + RIOT_API_KEY;
-        
-        axios.get(API_CALL_RANK).then(function(res) {
-            setPlayerData(res.data); 
-        }).catch(function(err) {
-            console.log(err)
-        })
-    }
-
-    let playerDataComponent;
-
-    if (playerData.length === 3) {
-        playerDataComponent = (
-            <>
-            <h1>{playerName.name}</h1>
-            <h1>{playerData[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
-            <h1>{playerData[0].tier}</h1>
-            <h1>{playerData[0].rank}</h1>
-            <h1>{playerData[1].queueType.split("_").slice(0, 2).join(" ")}</h1>
-            <h1>{playerData[1].tier}</h1>
-            <h1>{playerData[1].rank}</h1>
-            <h1>{playerData[2].queueType.split("_").slice(0, 2).join(" ")}</h1>
-            <h1>{playerData[2].tier}</h1>
-            <h1>{playerData[2].rank}</h1>
-            </>
-        )
-    } else if (playerData.length === 2) {
-        playerDataComponent = (
-            <>
-            <h1>{playerData[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
-            <h1>{playerData[0].tier}</h1>
-            <h1>{playerData[0].rank}</h1>
-            <h1>{playerData[1].queueType.split("_").slice(0, 2).join(" ")}</h1>
-            <h1>{playerData[1].tier}</h1>
-            <h1>{playerData[1].rank}</h1>      
-            </>
-        )
-    } else if (playerData.length === 1) {
-        playerDataComponent = (
-            <>
-            <h1>{playerData[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
-            <h1>{playerData[0].tier}</h1>
-            <h1>{playerData[0].rank}</h1>
-            </>
-        )
-    } else {
-        playerDataComponent = (
-            <>
-            <h1>Rank not Found!</h1>
-            </>
-        )
-    }
-
-    return(
-        <>
+        .then(res => res.json())
+        .then(data => {
+            if (data.length === 3) {
+                setPlayerRankComponent(
+                    <>
+                <h1>{data[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                <h1>{data[0].tier}</h1>
+                <h1>{data[0].rank}</h1>
+                <h1>{data[1].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                <h1>{data[1].tier}</h1>
+                <h1>{data[1].rank}</h1>
+                <h1>{data[2].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                <h1>{data[2].tier}</h1>
+                <h1>{data[2].rank}</h1>
+                </>
+                )
+            } else if (data.length === 2) {
+                setPlayerRankComponent(
+                    <>
+                <h1>{data[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                <h1>{data[0].tier}</h1>
+                <h1>{data[0].rank}</h1>
+                <h1>{data[1].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                <h1>{data[1].tier}</h1>
+                <h1>{data[1].rank}</h1>      
+                </>
+                )
+            } else if (data.length === 1) {
+                setPlayerRankComponent(
+                    <>
+                <h1>{data[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                <h1>{data[0].tier}</h1>
+                <h1>{data[0].rank}</h1>
+                </>
+                )} 
+            })
+            .catch(err => {
+                console.error('Request Failed', err)
+            })
+    }, [])
+            return(
+                <>
         <div className='game-main-container'>
+            <div>
+                {playerInfoComponent}
+                {playerRankComponent}
+            </div>
             <div className='user-profile-box'>
                 <div className='image-box'>
                     <img id='profile-image' src={profile}/>
@@ -110,22 +105,6 @@ const Profile = () => {
                         </label>
                     </div>
                 </div>  
-            </div>
-            <div>
-            <button onClick={e => searchForPlayer(e)}>Search</button>
-            {JSON.stringify(playerName) != '{}' ? 
-                <>
-                <h1>{playerName.name}</h1>
-                <img src={'http://ddragon.leagueoflegends.com/cdn/12.18.1/img/profileicon/' + playerName.profileIconId + ".png" }></img>
-                <h2>{playerName.summonerLevel}</h2>
-                </>
-            :
-                <>
-                <h1>Player not found!</h1>
-                </>
-            }
-            <button onClick={e => getRankForPlayer(e)}>Get Rank</button>
-            {playerDataComponent}
             </div>
             <div className='user-reviews-box'>
                 <ReviewIndex/>
