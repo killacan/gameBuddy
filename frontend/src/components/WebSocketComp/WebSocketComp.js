dc954b731b65bbfa4f7d21d847ea175cb5f00d89
killacan <kameron000@yahoo.com>
2022-09-21 21:24:37 -0700

    added live chat

--- frontend/src/components/WebSocketComp/WebSocketComp.js
+++ frontend/src/components/WebSocketComp/WebSocketComp.js
@@ -6,17 +6,20 @@ import { useParams } from 'react-router-dom';
 
 function WebSocketComp () {
     const dispatch = useDispatch();
-    const {roomId} = useParams;
+    const {roomId} = useParams();
 
     const [message, setMessage] = useState("");
-    const [socket, setSocket] = useState(new WebSocket('ws://localhost:5000'));
+    const [socket, setSocket] = useState();
+    const [messages, setMessages] = useState([]);
     
 
 
     useEffect(() => {
         // const messages = document.getElementById('messages');
+        const socketNew = new WebSocket(`ws://localhost:5000/?roomId=${roomId}`);
 
-        socket.onopen = (event) => {
+
+        socketNew.onopen = (event) => {
             console.log('Websocket is connected!')
             const id = Math.round(Math.random() * 100)
             console.log('sending...', id)
@@ -58,33 +61,42 @@ function WebSocketComp () {
             // }, 2000)
         }
 
-        socket.onmessage = (message) => {
-            const mesData = JSON.parse(message)
-            console.log('I got a message', mesData);
+        socketNew.onmessage = (message) => {
+            console.log('I got a message', JSON.parse(message.data));
+            setMessages(messages => [...messages, JSON.parse(message.data)])
             // messages.innerHTML += `<br/>${message}`
         }
 
-        socket.onerror = (error) => console.log(`WebSocket error: ${error}`);
-        socket.onclose = (event) => console.log('Disconnected from WebSocket server');
-
-        return () => socket.close()
+        socketNew.onerror = (error) => console.log(`WebSocket error: ${error}`);
+        socketNew.onclose = (event) => socket.send(JSON.stringify({type: 'close', data: 'bye'}));
+        setSocket(socketNew);
+        return () => socketNew.close()
     }, [])
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('sending...', message)
-        socket.send({message: message, roomId: roomId})
+        socket.send(JSON.stringify({message: message, roomId: roomId}))
     }
 
     if (!socket) return null;
 
     return (
         <>
-            <output id="messages"></output>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} className='message-input' placeholder='Type a message...' />
-                <button type="submit">Send</button>
-            </form>
+            <div className='chat-container'>
+                <div className='room-messages-container'>
+                    {messages.map (message => {
+                        return <div>{message.message}</div>
+                        })
+                    }
+                </div>
+                <div className='room-input-container'>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} className='message-input' placeholder='Type a message...' />
+                        <button type="submit" className='message-submit'>Send</button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
