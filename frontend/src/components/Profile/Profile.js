061a03a5bf8213d34891584f78ac5f0d58cf7a2a
Mimi <lyoanhmymimi@gmail.com>
2022-09-22 17:46:16 -0700

    design profile page with api ranks

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -30,12 +30,21 @@ const Profile = () => {
             const playerId = data.id;
             if (playerId) {
                 setPlayerInfoComponent (
-                    <>
-                    <img src={'http://ddragon.leagueoflegends.com/cdn/12.18.1/img/profileicon/' + data.profileIconId + ".png" }></img>
-                    <h1>League Username:{data.name}</h1>
-                    <h2>Summoner Level:{data.summonerLevel}</h2>
-                    </>
+                    <div className="icon-img">
+                        <div id="in-game-icon">
+                            <img id="in-game-img" src={'http://ddragon.leagueoflegends.com/cdn/12.18.1/img/profileicon/' + data.profileIconId + ".png" }></img>
+                        </div>
+                        <div className="league-summoner-container">
+                            <h1 id="league-username">League Username : {data.name}</h1>
+                            <h2 id="summoner-lvl">Summoner Level : {data.summonerLevel}</h2>
+                        </div>
+                    </div>
                 )            
+            } else {
+                setPlayerInfoComponent (
+                    <>
+                    </>
+                )
             }
             return fetch("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"
             + playerId + "?api_key=" + RIOT_API_KEY)
@@ -44,60 +53,61 @@ const Profile = () => {
         .then(data => {
             if (data.length === 3) {
                 setPlayerRankComponent(
-                    <>
-                <h1>{data[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
-                <h1>{data[0].tier}</h1>
-                <h1>{data[0].rank}</h1>
-                <h1>{data[1].queueType.split("_").slice(0, 2).join(" ")}</h1>
-                <h1>{data[1].tier}</h1>
-                <h1>{data[1].rank}</h1>
-                <h1>{data[2].queueType.split("_").slice(0, 2).join(" ")}</h1>
-                <h1>{data[2].tier}</h1>
-                <h1>{data[2].rank}</h1>
-                </>
+                <div id="player-rank-component">
+                    <h1 id="ranked-solo">{data[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                    <h1>{data[0].tier}</h1>
+                    <h1>{data[0].rank}</h1>
+                    <h1 id="ranked-tft">{data[1].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                    <h1>{data[1].tier}</h1>
+                    <h1>{data[1].rank}</h1>
+                    <h1 id="ranked-flex">{data[2].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                    <h1>{data[2].tier}</h1>
+                    <h1>{data[2].rank}</h1>
+                </div>
                 )
             } else if (data.length === 2) {
                 setPlayerRankComponent(
-                    <>
-                <h1>{data[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
-                <h1>{data[0].tier}</h1>
-                <h1>{data[0].rank}</h1>
-                <h1>{data[1].queueType.split("_").slice(0, 2).join(" ")}</h1>
-                <h1>{data[1].tier}</h1>
-                <h1>{data[1].rank}</h1>      
-                </>
+                <div  id="player-rank-component">
+                    <h1 id="unknown-1">{data[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                    <h1>{data[0].tier}</h1>
+                    <h1>{data[0].rank}</h1>
+                    <h1 id="unknown-2">{data[1].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                    <h1>{data[1].tier}</h1>
+                    <h1>{data[1].rank}</h1>      
+                </div>
                 )
             } else if (data.length === 1) {
                 setPlayerRankComponent(
-                    <>
-                <h1>{data[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
-                <h1>{data[0].tier}</h1>
-                <h1>{data[0].rank}</h1>
-                </>
+                <div  id="player-rank-component">
+                    <h1 id="unknown-3">{data[0].queueType.split("_").slice(0, 2).join(" ")}</h1>
+                    <h1>{data[0].tier}</h1>
+                    <h1>{data[0].rank}</h1>
+                </div>
                 )} 
             })
             .catch(err => {
                 console.error('Request Failed', err)
             })
     }, [])
-            return(
-                <>
-        <div className='game-main-container'>
-            <div>
-                {playerInfoComponent}
-                {playerRankComponent}
-            </div>
-            <div className='user-profile-box'>
-                <div className='image-box'>
-                    <img id='profile-image' src={profile}/>
+    return(
+        <>
+            <div className='game-main-container'>
+
+                <div className='user-profile-box'>
+                    <div className='img-box'>
+                        {playerInfoComponent}
+                    </div>
+                    <div className="rank-info">
+                        {playerRankComponent}
+                    </div>
+                </div>
+
+                <div className='user-reviews-box'>
+                    <ReviewIndex/>
                 </div>
             </div>
-            <div className='user-reviews-box'>
-                <ReviewIndex/>
-            </div>
-        </div>
         </>
-    )
+)
 }
 
 export default Profile;
