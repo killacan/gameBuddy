c9f4161bd9c8f283f0e9f4dc3e9b140401d6c65f
daniellien2018 <daniellien2018@gmail.com>
2022-10-04 11:22:21 -0700

    timerdone-noRoomDestroy

--- frontend/src/components/GameRoom/Utils/CountdownTimerUtils.js
+++ frontend/src/components/GameRoom/Utils/CountdownTimerUtils.js
@@ -1,14 +1,22 @@
 import dayjs from "dayjs"
+import { useParams } from "react-router-dom";
+import { destroyRoom } from "../../../store/rooms";
+import { useSelector } from "react-redux";
 
 
-const getRemainingTimeUntilMsTimestamp= (timestampMs) => {
+const getRemainingTimeUntilMsTimestamp = (timestampMs) => {
     const timestampDayjs = dayjs(timestampMs)
     const nowDayjs = dayjs();
+
+    // const {roomId} = useParams();
+    // const room = useSelector(state => state.rooms[roomId]);
+
     if (timestampDayjs.isBefore(nowDayjs)){
         return {
             seconds: "00",
             minutes: "00"
         }
+        // dispatchEvent(destroyRoom(roomId))
     }
     return {
         seconds: getRemainingSeconds(nowDayjs, timestampDayjs),
