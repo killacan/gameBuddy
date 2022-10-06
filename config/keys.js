e90c2d0e8d15d39580aa295fbc78a1539fbb81d2
mattzteh <mattzteh@gmail.com>
2022-10-06 15:17:53 -0700

    add options

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     secretOrKey: process.env.SECRET_OR_KEY,
     mongoURI: process.env.MONGO_URI,
-    isProduction: process.env.NODE_ENV === 'production'
+    isProduction: process.env.NODE_ENV === 'production',
+    reactAppRiotApiKey: process.env.REACT_APP_RIOT_API_KEY
 }
